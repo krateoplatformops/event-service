a7b739464b7f42913b7c31817aed8b3c021b1a7c
mauro sala <mauro.sala@gmail.com>
2022-05-13 09:10:16 +0200

    fix(status): rename

--- routes/status.routes.js
+++ routes/status.routes.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/ping', (req, res) => {
-  res.status(200).send('Template Service')
+  res.status(200).send('Log Service')
 })
 
 router.get('/healthz', (req, res) => {
-  res.status(200).send('Template Service')
+  res.status(200).send('Log Service')
 })
 
 module.exports = router
